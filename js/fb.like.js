c2fc929046dabd6c95e6c6057726be5a9e018725
Kris Zhou <kris.zhou@autodesk.com>
2013-04-26 14:32:21 +0800

    FB event attachment

--- js/fb.like.js
+++ js/fb.like.js
@@ -1,7 +1,29 @@
-<script>(function(d, s, id) {
+window.fbAsyncInit = function() {
+// init the FB JS SDK
+/*
+	FB.init({
+	  appId      : 'YOUR_APP_ID',                        // App ID from the app dashboard
+	  channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel file for x-domain comms
+	  status     : true,                                 // Check Facebook Login status
+	  xfbml      : true                                  // Look for social plugins on the page
+	});
+*/
+// Additional initialization code such as adding Event Listeners goes here
+	FB.Event.subscribe('edge.create',
+	    function(response) {
+	    	var product = response.substring(response.indexOf('#'));
+	    	util.tracking.linkClick('fb-like', product);
+	    }
+	);
+};
+
+  // Load the SDK asynchronously
+
+(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
   js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
   fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+}(document, 'script', 'facebook-jssdk'));
+
